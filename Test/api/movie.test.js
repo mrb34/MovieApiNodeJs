e127879248b68154cbc44b08c431d853bf3bbda3
Yusuf ŞENEL <ysf.senel@gmail.com>
2019-09-29 14:30:03 +0300

    test director movie

---
+++ Test/api/movie.test.js
@@ -0,0 +1,130 @@
+const chai=require('chai');
+const chaiHttp=require('chai-http');
+const should=chai.should();
+const server=require('../../app');
+
+chai.use(chaiHttp);
+let token,movieId;
+
+describe('api/movies tests',()=>{
+    // burda before ile  test kullnıcı bilgleri ile authentication için token bilgileri alınır
+before((done)=>{
+    chai.request(server)
+        .post('/authenticate')
+        .send({username:'yusuf',password:'12345'})
+        .end((err,res)=>{
+        token=res.body.token;
+
+        done();
+        });
+
+});
+describe('GET/ movies',()=>{
+    it('it should GET all movies',(done)=>{
+        //burda alınan token ile get isteğinde bulunulur ve 200 kodu dönmesi beklenir.
+        chai.request(server)
+            .get('/api/movies')
+            .set('x-access-token',token)
+            .end((err,res)=>{
+                    res.should.have.status(200);
+                    res.body.should.be.a('array');
+                    done();
+            });
+    });
+});
+describe('/Post movie',()=>{
+    it('it should be post a movie',  (done)=> {
+        const movie={
+           title:'udemy',
+           director_id:'5d8a5d25a27d4649a8d303e3',
+            category:'komedi',
+            country:'türkiye',
+            year:1950,
+            imdb_score:6
+
+        };
+    chai.request(server)
+        .post('/api/movies')
+        .send(movie)
+        .set('x-access-token',token)
+        .end((err,res)=>{
+            res.should.have.status(200);
+            res.body.should.be.a('object');
+            res.body.should.have.property('title');
+            res.body.should.have.property('director_id');
+            res.body.should.have.property('category');
+            res.body.should.have.property('country');
+            res.body.should.have.property('year');
+            res.body.should.have.property('imdb_score');
+            movieId=res.body._id;
+            done();
+        });
+    });
+
+});
+describe('/GET/movie_id movie',()=>{
+    it('it should GET a movie by the given id ',  (done)=> {
+        chai.request(server)
+        .get('/api/movies/'+movieId)
+            .set('x-access-token',token)
+            .end((err,res)=>{
+                res.should.have.status(200);
+                res.body.should.be.a('object');
+                res.body.should.have.property('title');
+                res.body.should.have.property('director_id');
+                res.body.should.have.property('category');
+                res.body.should.have.property('country');
+                res.body.should.have.property('year');
+                res.body.should.have.property('_id').eql(movieId);
+                done();
+            });
+    });
+    });
+describe('/PUT/movie_Id movie',()=>{
+        it('it should UPDATE a movie given by id',  (done)=> {
+            const movie={
+                title:'86creative',
+                director_id:'5d8a5d25a27d4649a8d303e1',
+                category:'suç',
+                country:'Fransa',
+                year:1970,
+                imdb_score:9
+
+            };
+            chai.request(server)
+                .put('/api/movies/'+movieId)
+                .send(movie)
+                .set('x-access-token',token)
+                .end((err,res)=>{
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('title').eql(movie.title);
+                    res.body.should.have.property('director_id').eql(movie.director_id);
+                    res.body.should.have.property('category').eql(movie.category);
+                    res.body.should.have.property('country').eql(movie.country);
+                    res.body.should.have.property('year').eql(movie.year);
+                    res.body.should.have.property('imdb_score').eql(movie.imdb_score);
+
+                    done();
+                });
+        });
+
+    });
+
+describe('DELETE/movie_id movie',()=>{
+    it('should be DELETE a movie given by id',  (done)=> {
+
+        chai.request(server)
+            .delete('/api/movies/'+movieId)
+            .set('x-access-token',token)
+            .end((err,res)=>{
+                res.should.have.status(200);
+                res.body.should.be.a('object');
+                res.body.should.have.property('status').eql(1);
+                done();
+            });
+
+    });
+});
+});
+
---
+++ routes/movie.js
@@ -0,0 +1,136 @@
+const express = require('express');
+const router = express.Router();
+//Models
+const Movie=require('../Models/Movie');
+
+////////////////////LIST ALL METHOD///////////////////////////////////////////////
+router.get('/',(req,res)=>{
+const promise=Movie.aggregate([
+  {
+    $lookup:{
+      from:'directors',
+      localField:'director_id',
+      foreignField:'_id',
+      as:'director'
+    }
+  }
+  ,
+  {
+    $unwind:'$director'
+  }
+]);
+promise.then((data)=>{
+  res.json(data);
+}).catch((err)=>{
+  res.json(err);
+})
+});
+//////////////////////TOP10//////////////////////////////////////////
+router.get('/top10',(req,res)=>{
+  const promise=Movie.find({ }).limit(10).sort({imdb_score:-1});
+  promise.then((data)=>{
+    res.json(data);
+  }).catch((err)=>{
+    res.json(err);
+
+  })
+});
+////////////////////BETWEEN////////////////////////////////////
+router.get('/between/:start_year/:end_year',(req,res)=>{
+  const {start_year,end_year}=req.params;
+  const promise=Movie.find({
+    year:{"$gte":parseInt(start_year),"$lte":parseInt(end_year)}
+    //gte great and equal
+    //lte little and equal
+    //gt great
+    //lt little
+
+  });
+  promise.then((data)=>{
+    res.json(data);
+  }).catch((err)=>{
+    res.json(err);
+  })
+});
+///////////////////////////UPDATE METHOD///////////////////////////////////////
+router.put('/:movie_id',(req,res,next)=>{
+  const  promise=Movie.findByIdAndUpdate(
+      req.params.movie_id,
+      req.body,
+      {
+        new:true
+      }
+);
+  promise.then((movie)=>{
+    if (!movie){
+      next({message:'The movie was not found',code:2});
+    }else {
+      res.json(movie);
+     // res.json({status:1});
+    };
+  }).catch((err)=>{
+    res.json(err);
+  });
+});
+////////////////////////LİST WITH ID METHOD//////////////////////////////////////////////
+
+router.get('/:movie_id',(req,res,next)=>{
+  const  promise=Movie.findById(req.params.movie_id);
+  promise.then((movie)=>{
+    if (!movie){
+      next({message:'The movie was not found',code:1});
+    }else {
+      res.json(movie);
+    };
+  }).catch((err)=>{
+    res.json(err);
+  });
+});
+
+////////////////ADD METHOD//////////////////////////////////
+router.post('/', (req, res, next)=> {
+  //const {title,imdb_score,category,country,year}=req.body;
+/*  const movie=new Movie({
+    title:title,
+    imdb_score:imdb_score,
+    category:category,
+    country:country,
+    year:year
+  });
+  */
+
+  const movie=new Movie(req.body);
+/*  movie.save((err,data)=>{
+    if (err)
+      res.json(err);
+   // res.json(data);
+    res.json(data); //res.json({status:1});
+  });*/
+const promise=movie.save();
+promise.then((data)=>{
+ // res.json({status:1});
+  res.json(data);
+}).catch((err)=>{
+  res.json(err);
+});
+
+});
+//////////////////DELETE METHOD//////////////////////////////////////////
+
+router.delete('/:movie_id',(req,res,next)=>{
+  const  promise=Movie.findByIdAndRemove(req.params.movie_id);
+  promise.then((movie)=>{
+    if (!movie){
+      next({message:'The movie was not found',code:3});
+    }else {
+    //  res.json(movie);
+      res.json({status:1});
+    };
+  }).catch((err)=>{
+    res.json(err);
+  });
+});
+
+
+
+module.exports = router;
